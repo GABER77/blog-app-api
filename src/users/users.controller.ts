34eabd7984ce30a98fca46ea99e5bdf5858a4cca
GABER77 <mg7965724@gmail.com>
2025-06-16 21:32:16 +0300

    Ensure correct parameter types using ParseIntPipe and DefaultValuePipe in getUsers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id{/:optional}')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  @Get('{/:id}')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(typeof id, id);
+    console.log(typeof limit, limit);
+    console.log(typeof page, page);
     return 'Get Users Endpoint';
   }
 
