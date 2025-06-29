6acaa39afa054f899a0521f4c4d8303efda9108d
GABER77 <mg7965724@gmail.com>
2025-06-29 23:39:58 +0300

    refactor: separate TypeORM config into dedicated config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,27 +7,17 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TagsModule } from './tags/tags.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     // Load environment variables globally
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule], // Let TypeOrmModule use environment variables
       inject: [ConfigService], // Inject ConfigService to read .env values
-
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true, // auto-creates tables in dev (DON'T USE IT IN PRODUCTION)
-      }),
+      useFactory: typeOrmConfig,
     }),
     UsersModule,
     PostsModule,
