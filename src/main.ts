ff0c6d804a7a63ea5085d71db8a2318372c8196c
GABER77 <mg7965724@gmail.com>
2025-07-15 17:34:13 +0300

    refactor: move middleware setup from main.ts to app.create.ts

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,18 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
-import * as cookieParser from 'cookie-parser';
-import * as AWS from 'aws-sdk';
-import { DataResponseInterceptor } from './common/interceptors/data-response.interceptor';
+import { appCreate } from './app.create';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Allow requests from other domains (Cross-Origin Resource Sharing)
-  app.enableCors({
-    origin: ['http://localhost:3000'], // Frontend origin
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // // enable cookies and JWTs with credentials
-  });
-
-  // Enable global validation pipe with strict rules
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // removes properties not defined in your DTO
-      forbidNonWhitelisted: true, // throws error if extra fields are present
-      stopAtFirstError: true, // stops showing multiple errors per property
-      transform: true, // transforms incoming request to match the DTO type
-      transformOptions: {
-        enableImplicitConversion: true, // Enable automatic type conversion for incoming request
-      },
-    }),
-  );
-
-  // Global Exception Filter
-  // Handle DB related errors (e.g. failed .findOne, .save, .create, etc.)
-  app.useGlobalFilters(new GlobalExceptionFilter());
-
-  // Global Interceptors
-  const reflector = app.get(Reflector);
-  app.useGlobalInterceptors(
-    // Remove fields marked with @Exclude() in your entity when returning responses
-    new ClassSerializerInterceptor(reflector),
-    new DataResponseInterceptor(),
-  );
-
-  // Initialize AWS S3 as runtime assignment of a global variable
-  globalThis.s3 = new AWS.S3({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    },
-  });
-
-  // Swagger Configuration
-  const config = new DocumentBuilder()
-    .setTitle('Blog App API')
-    .setDescription(
-      'RESTful API for managing users, authentication, posts, and comments in a modular blog application.',
-    )
-    .addServer('http://localhost:3000')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  // Reading data from the cookies (req.cookies)
-  app.use(cookieParser());
+  // Add all middleware
+  appCreate(app);
 
   // Start the application
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 8000;
+  await app.listen(port).then(() => {
+    console.log(`🚀 App running on port ${port}`);
+  });
 }
 
 void bootstrap();
--- src/users/services/user.service.spec.ts
+++ src/users/services/user.service.spec.ts
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
 
 // Utility to mock the TypeORM repository methods
+// Because we don't want to run real DB queries
+// Just simulate what the DB would do
 const mockUserRepo = () => ({
   findOne: jest.fn(),
   findOneBy: jest.fn(),
@@ -15,14 +17,11 @@ const mockUserRepo = () => ({
   update: jest.fn(),
 });
 
-jest.mock('../../common/upload/upload.service', () => ({
-  uploadImage: jest.fn(),
-}));
-
 describe('UserService', () => {
   let service: UserService;
-  let repo: jest.Mocked<Repository<User>>;
+  let mockedRepo: jest.Mocked<Repository<User>>;
 
+  // Creating a testing environment (a mini NestJS app)
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -35,7 +34,7 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
-    repo = module.get(getRepositoryToken(User));
+    mockedRepo = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
@@ -45,14 +44,16 @@ describe('UserService', () => {
   describe('getUserById', () => {
     it('should return user if found', async () => {
       const user = { id: '123', email: 'test@test.com' } as User;
-      repo.findOneBy.mockResolvedValueOnce(user);
+      // You tell the mock repo to return user when findOneBy() is called
+      mockedRepo.findOneBy.mockResolvedValueOnce(user);
 
+      // Calls your real service method, which uses the mocked repo
       const result = await service.getUserById('123');
       expect(result).toEqual(user);
     });
 
     it('should throw NotFoundException if user not found', async () => {
-      repo.findOneBy.mockResolvedValueOnce(null);
+      mockedRepo.findOneBy.mockResolvedValueOnce(null);
 
       await expect(service.getUserById('123')).rejects.toThrow(
         NotFoundException,
@@ -68,16 +69,16 @@ describe('UserService', () => {
       const updatedUser = { id: userId, name: 'Updated Name' } as User;
 
       // Simulate finding the user and saving changes
-      repo.findOne.mockResolvedValueOnce(existingUser); // For existence check
-      repo.save.mockResolvedValueOnce(updatedUser);
-      repo.findOne.mockResolvedValueOnce(updatedUser); // For final return
+      mockedRepo.findOne.mockResolvedValueOnce(existingUser); // For existence check
+      mockedRepo.save.mockResolvedValueOnce(updatedUser);
+      mockedRepo.findOne.mockResolvedValueOnce(updatedUser); // For final return
 
       const result = await service.updateUser(userId, updateData);
       expect(result).toEqual(updatedUser);
     });
 
     it('should throw NotFoundException if user not found', async () => {
-      repo.findOne.mockResolvedValueOnce(null);
+      mockedRepo.findOne.mockResolvedValueOnce(null);
       await expect(service.updateUser('123', { name: 'John' })).rejects.toThrow(
         NotFoundException,
       );
